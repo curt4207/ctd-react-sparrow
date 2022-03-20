158d83d685fb13d169f46063c766133f5a792260
Curtis <Curt122181@gmail.com>
2022-03-20 19:49:07 -0400

    Start of Todo List app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const title = "Todo List";
+function App() {
+
+  return (
+    <div>
+      <h1>{title}</h1>
+
+    <label htmlFor="search ">Search:</label>
+    <input id="search" type="text"></input>
+
+    </div>
+   );
+ 
+}
+
+export default App;
